066166fa260093167302679cb8aa2018ceadc862
Siyavash Feyzi <58208265+siyavashfeyzi@users.noreply.github.com>
2021-08-31 15:28:59 +0430

    Create static Class and methodes.

---
+++ Caesar_Cipher.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftCipher
+{
+    public static class Caesar_Cipher
+    {
+        #region Encrypt
+        /// <summary>
+        /// This function takes the text with a key and returns the encrypted text.
+        /// </summary>
+        /// <param name="Text"></param>
+        /// <param name="Key"></param>
+        /// <returns>encryptedText</returns>
+        public static string Encrypt(string Text, int Key) 
+        {
+            string text = Text;
+            int key = Key;
+
+            int lengthText = text.Length;
+            int[] numberOfLetters = new int[lengthText];
+            char[] Code = new char[lengthText];
+
+            char[] Alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            // Find whole text numbers
+            for (int i = 0; i < lengthText; i++)
+            {
+                char temp = text[i];
+                // Find the number of each letter
+                for (int j = 0; j < 26; j++)
+                {   //Alphabet[j] == temp
+                    if (temp.Equals(Alphabet[j]))
+                    {
+                        numberOfLetters[i] = j;
+
+                    }
+                }
+            }
+
+            //Convert numbers
+            for (int i = 0; i < lengthText; i++)
+            {
+                numberOfLetters[i] += key;
+                if (numberOfLetters[i] > 25)
+                {
+                    numberOfLetters[i] -= 26;
+                }
+            }
+
+            //Convert numbers to letters
+            for (int i = 0; i < lengthText; i++)
+            {
+                Code[i] = Alphabet[numberOfLetters[i]];
+            }
+
+            string encryptedText = new string(Code);
+            return encryptedText;
+        }
+        #endregion
+
+        #region Decrypt
+        /// <summary>
+        /// This function takes the encrypted text with the key and returns the decrypted text.
+        /// </summary>
+        /// <param name="Code"></param>
+        /// <param name="Key"></param>
+        /// <returns>decryptedText</returns>
+        public static string Decrypt(string Code, int Key)
+        {
+            string code = Code;
+            int key = Key;
+
+            int lengthCode = code.Length;
+            int[] numberOfLetters = new int[lengthCode];
+            char[] text = new char[lengthCode];
+
+            char[] Alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            // Find whole text numbers
+            for (int i = 0; i < lengthCode; i++)
+            {
+                char temp = code[i];
+                // Find the number of each letter
+                for (int j = 0; j < 26; j++)
+                {   //Alphabet[j] == temp
+                    if (temp.Equals(Alphabet[j]))
+                    {
+                        numberOfLetters[i] = j;
+
+                    }
+                }
+            }
+
+            //Convert numbers
+            for (int i = 0; i < lengthCode; i++)
+            {
+                numberOfLetters[i] -= key;
+                if (numberOfLetters[i] < 0)
+                {
+                    numberOfLetters[i] += 26;
+                }
+            }
+
+            //Convert numbers to letters
+            for (int i = 0; i < lengthCode; i++)
+            {
+                text[i] = Alphabet[numberOfLetters[i]];
+            }
+            string decryptedText = new string(text);
+            return decryptedText;
+        }   
+        #endregion
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,62 +6,56 @@ namespace ShiftCipher
     {
         private static void Main(string[] args)
         {
-            int Continue = 0;
+            int Key;
+            string Text;
+            string Code;
+            string answer;
+            ConsoleKey response;
+            bool Continues = false;
+            
             //Repeat program loop
             do
             {
-                Console.WriteLine("Select the desired option: \n1.Encrypt\n2.Decrypt");
-                int Select = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Select the desired option Encrypt or Decrypt? [e/d]");
+                string Select = Console.ReadLine().ToLower();
 
                 switch (Select)
                 {
-                    case 1:
+                    case "e":
 
                         Console.WriteLine("enter your text: ");
-                        string p0 = Console.ReadLine();
-                        string p = p0.ToLower();
+                        Text = Console.ReadLine().ToLower();
+
                         Console.WriteLine("enter your key: ");
-                        int Key = Convert.ToInt32(Console.ReadLine());
+                        Key = Convert.ToInt32(Console.ReadLine());
 
-                        Encrypt ek = new Encrypt
-                        {
-                            key = Key,
-                            p = p
-                        };
-                        ek.EkMethod();
-
+                        answer = Caesar_Cipher.Encrypt(Text, Key);
+                        Console.WriteLine(answer);
                         break;
 
-                    case 2:
+                    case "d":
 
                         Console.WriteLine("enter your text: ");
-                        string c0 = Console.ReadLine();
-                        string c = c0.ToLower();
+                        Code = Console.ReadLine().ToLower();
+
                         Console.WriteLine("enter your key: ");
-                        int key = Convert.ToInt32(Console.ReadLine());
+                        Key = Convert.ToInt32(Console.ReadLine());
 
-                        Decrypt dk = new Decrypt
-                        {
-                            Code = c,
-                            key = key
-                        };
-                        dk.DkMethod();
-
+                        answer = Caesar_Cipher.Decrypt(Code, Key);
+                        Console.WriteLine(answer);
                         break;
 
                 }
 
                 Console.WriteLine();
-                Console.WriteLine("Do you want to continue?\n1.Yes\n2.No");
-                Continue = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Do you want to continue? [y/n]");
+                response = Console.ReadKey(true).Key;
 
-            } while (Continue==1);
+                Continues = response == ConsoleKey.Y ? true : false;
 
+            } while (Continues);
 
         }
-
-
-
 
     }
 }
